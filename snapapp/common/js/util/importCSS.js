b6208fa5c7faaf8c5330502054ae3a1c4e0109b9
Deheeger Pierrick <pdeheeger@gmail.com>
2012-05-15 16:07:15 +0200

    template for importing Js et Css

---
+++ snapapp/common/js/util/importCSS.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Pierrick
+ * Date: 15/05/12
+ * Time: 14:43
+ * To change this template use File | Settings | File Templates.
+ */
+
+$("head").append('<link href="/snapapp/common/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">');
+$("head").append('<link href="/snapapp/common/lib/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">');
---
+++ snapapp/common/js/util/importJS.js
@@ -0,0 +1,19 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Pierrick
+ * Date: 15/05/12
+ * Time: 11:43
+ * To change this template use File | Settings | File Templates.
+ */
+
+//<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+//[if lt IE 9]>
+    $("head").append('<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>');
+//[endif]-->
+
+$("head").append('<script src="/snapapp/common/lib/angular/angular.js"></script>');
+$("head").append('<script src="/snapapp/common/js/models/facebook/facebook.js"></script>');
+$("head").append('<script src="/snapapp/common/js/models/google+/google+.js"></script>');
+$("head").append('<script src="/snapapp/common/js/models/mocks/mocks.js"></script>');
+$("head").append('<script src="/snapapp/common/js/models/twitter/twitter.js"></script>');
+$("head").append('<script src="/snapapp/common/js/models/socialNetworks.js"></script>');
