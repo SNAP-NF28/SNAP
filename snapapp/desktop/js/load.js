621a72512afaafd2423d3768a1ad45283dbb63d4
Charles de Bellabre <charles.debellabre@gmail.com>
2012-05-24 16:10:45 +0200

    Erreur corrigée

--- snapapp/desktop/js/load.js
+++ snapapp/desktop/js/load.js
@@ -1,7 +1,7 @@
 require({paths: {order: "/snapapp/common/lib/requirejs/order"}},
     commonScripts.concat([
     //List here script specific to desktop
-    "order!/snapapp/desktop/js/desktopApp.js",	
+    "order!/snapapp/desktop/js/desktopapp.js",	
 ]), function() {
     angular.bootstrap(document, ['desktopApp']);
 });
