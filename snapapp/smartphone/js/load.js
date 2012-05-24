ed4520306b3669ac35d658112839959ea27a3c20
Kim-Adeline Miguel <kimadeline.miguel@gmail.com>
2012-05-24 14:18:52 +0200

    Angular + JQueryMobile : OK

--- snapapp/smartphone/js/load.js
+++ snapapp/smartphone/js/load.js
@@ -1,6 +1,7 @@
 require({paths: {order: "/snapapp/common/lib/requirejs/order"}},
     commonScripts.concat([
-        //List here script specific to desktop
+        //List here script specific to smartphone
+		"order!/snapapp/smartphone/js/jquery.mobile-1.1.0.js",
         "order!/snapapp/smartphone/js/smartphoneApp.js"
     ]), function() {
         angular.bootstrap(document, ['smartphoneApp']);
