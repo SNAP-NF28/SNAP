b91aefc6596000f15b1f2b210ef5a33fed97bc4c
Kim-Adeline Miguel <kimadeline.miguel@gmail.com>
2012-05-29 19:31:04 +0200

    Implementation de la navigation

--- snapapp/smartphone/js/smartphoneApp.js
+++ snapapp/smartphone/js/smartphoneApp.js
@@ -1,5 +1,28 @@
 angular.module('smartphoneApp', ['socialNetworks']);
 
+
 function smartphoneAppCtrl($scope, SocialNetworks) {
     $scope.socialNetworks = new SocialNetworks();
+	
 }
+
+function smartphoneAppCtrlMsg($scope) {
+$scope.pageOrder = new Array("homePage", "fbPage", "twPage", "gpPage", "srchPage", "usrPage", "optionsPage");
+$scope.getPrevPage = function (curPage) {
+	var idx = $scope.pageOrder.indexOf(curPage);
+	if (idx <= 0) {
+		idx += $scope.pageOrder.length;
+	}
+	idx--;
+	return "#" + $scope.pageOrder[idx];
+}
+$scope.getNextPage = function (curPage) {
+	var idx = $scope.pageOrder.indexOf(curPage);
+	if (idx >=  $scope.pageOrder.length - 1) {
+		idx -= $scope.pageOrder.length;
+	}
+	idx++;
+	return "#" + $scope.pageOrder[idx];
+}
+}
+
