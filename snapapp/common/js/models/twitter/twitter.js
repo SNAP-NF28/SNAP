b570c6ba4b3538495e3d6c569b02879031900e68
Romain Yon <romain.yon@gmail.com>
2012-06-05 00:18:46 +0200

    update twitter user profile

--- snapapp/common/js/models/twitter/twitter.js
+++ snapapp/common/js/models/twitter/twitter.js
@@ -26,10 +26,12 @@ angular.module('twitter',['SNMock']).
 			SNMock.apply(this);
             //list attributes
             this.name = "Twitter";
+            this.id = 'twitter';
 			this.displayName = "Twitter";
             this.picture = "TwitPic";
             this.icon = "/snapapp/common/img/logo_twitter_60x60.png";
 			this.limitChar = 140;
+            this.profile = new Profile();
             return this;
         }
 		
@@ -57,7 +59,7 @@ angular.module('twitter',['SNMock']).
                 for(m in data.results){
                     var r = data.results[m];
                     var message = new Message();
-                    message.socialNetworkId = 'twitter';
+                    message.socialNetworkId = this.id;
                     message.msgId = r.id;
                     message.authorId = r.to_user_id_str;
                     message.msgContent = r.text;
@@ -92,27 +94,55 @@ angular.module('twitter',['SNMock']).
 			return msgList;
 		}
 
+        Twitter.prototype.getUserProfile = function(id){
+            return this.profile;
+        }
+
         Twitter.prototype.connect = function(){
 
             //FIXME: Set proper callback url
             //twttr.anywhere.config({ callbackURL: "http://www.yoursite.com/anywhere-complete" });
+            twttr.anywhere(function (T) {
+                if (T.isConnected()) {
+                    usr = T.currentUser;
+                    this.profile.socialNetworkId = this.id;
+                    this.profile.imageProfileURL = usr.profileImageUrl;
+                    this.profile.name = usr.screenName;
+                    this.profile.description = usr.description;
+                    this.profile.lifePlace = usr.location;
+                    this.profile.followersNb = usr.followersCount;
+                    this.profile.followsNb = usr.friendsCount;
+                    this.profile.msgCount = usr.statusesCount;
+                    this.profile.subscriptionDate = usr.createdAt;
+                    $("#login-Twitter").addClass("hide");
+                } else
+                if(!this.connectAlreadyCalled){ //FIXME: Ugly Ugly Ugly hack
+                    this.connectAlreadyCalled = true;
+                    twttr.anywhere(function (T) {
+                    T("#login-Twitter").connectButton({ //Fixme: use Twitter.name
+                      authComplete: function(usr) {
+                        // triggered when auth completed successfully
+                        this.profile.socialNetworkId = this.id;
+                        this.profile.imageProfileURL = usr.profileImageUrl;
+                        this.profile.name = usr.screenName;
+                        this.profile.description = usr.description;
+                        this.profile.lifePlace = usr.location;
+                        this.profile.followersNb = usr.followersCount;
+                        this.profile.followsNb = usr.friendsCount;
+                        this.profile.msgCount = usr.statusesCount;
+                        this.profile.subscriptionDate = usr.createdAt;
+                        $("#login-Twitter").addClass("hide");
+                        console.log("You rock baby");
+                      },
+                      signOut: function() {
+                        // triggered when user logs out
+                        console.log("You suck baby");
+                      }
+                    });
+                  });
+                }
+            });
 
-            if(!this.connectAlreadyCalled){ //FIXME: Ugly Ugly Ugly hack
-                this.connectAlreadyCalled = true;
-                twttr.anywhere(function (T) {
-                T("#login-Twitter").connectButton({ //Fixme: use Twitter.name
-                  authComplete: function(user) {
-                    // triggered when auth completed successfully
-                    var hahah = T;
-                    console.log("You rock baby");
-                  },
-                  signOut: function() {
-                    // triggered when user logs out
-                    console.log("You suck baby");
-                  }
-                });
-              });
-            }
             return ; //Must return connection object?
         }
 		
