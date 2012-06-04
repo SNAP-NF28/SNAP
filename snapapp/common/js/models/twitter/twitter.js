1967d09b2928a95cbdc6e99cef0933a8ec638724
Romain Yon <romain.yon@gmail.com>
2012-06-05 00:36:10 +0200

    update twitter user profile

--- snapapp/common/js/models/twitter/twitter.js
+++ snapapp/common/js/models/twitter/twitter.js
@@ -106,7 +106,7 @@ angular.module('twitter',['SNMock']).
             this.profile.msgCount = usr.statusesCount;
             this.profile.subscriptionDate = usr.createdAt;
             $("#login-Twitter").addClass("hide");
-            this.getLastNMessages(20);//FIXME: handle vary problem
+            //this.getLastNMessages(20);//FIXME: handle vary problem
         }
 
         Twitter.prototype.connect = function(){
