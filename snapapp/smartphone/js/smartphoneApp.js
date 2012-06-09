9fd726bd284c7f0c4e0970d592b92542ba3f16dc
Kim-Adeline Miguel <kimadeline.miguel@gmail.com>
2012-06-10 00:07:19 +0200

    smartphone : affichage mur all incomplet : fb & g+ not working --------- nettoyage & esthetique a finir

--- snapapp/smartphone/js/smartphoneApp.js
+++ snapapp/smartphone/js/smartphoneApp.js
@@ -6,15 +6,49 @@ function smartphoneAppCtrl($scope, SocialNetworks) {
 	$scope.socialNetworks = new SocialNetworks();
 	$('#msgDetails').live('pageshow', function () {displayMessage()});
 	//angular.element(document).scope().$apply(null); // force refresh view
-
+	
+	/** WTF 10 APPELS ???!! **/
+	$scope.getAllMessages = function(nb) { 
+		if ($scope.allMsg && $scope.allMsg.length > nb) {
+			console.log("all messages already sorted");
+			return $scope.allMsg;
+		} else {
+			console.log("get all messages");
+			
+			var tmp_1 = [];
+			for (var i = 0; i < $scope.socialNetworks.length; i++) {
+				var sn = $scope.socialNetworks[i];
+				var tmp = [];
+				if(sn.lastMessages && sn.lastMessages > nb) {
+					console.log("messages from " + sn.name + " already fetched");
+					tmp = sn.lastMessages;
+				} else {
+					//sn.alreadyFetched = true;
+					console.log("first time fetch from " + sn.name);
+					tmp = sn.getLastNMessages(20);
+				}
+				
+				console.log("fetch " + tmp.length + " messages from " + sn.name);
+				
+				for (var j = 0; j < tmp.length; j++) {
+					tmp_1.push(tmp[j]);
+				}
+			}
+			//$scope.alreadyFetched = true;
+			console.log("fetch done");
+			$scope.allMsg = tmp_1;
+		}
+		
+		return $scope.allMsg;
+	}
+	
 }
-
 /** Controleurs pour chaque reseau social **/
 
 function smartphoneFbCtrl($scope) {
 
 	$scope.socialNetwork = $scope.socialNetworks[0];
-	$scope.messages = $scope.socialNetwork.getLastNMessages(20);
+	//$scope.messages = $scope.socialNetwork.getLastNMessages(20);
 	$scope.getIcon = $scope.socialNetwork.icon;
 
 	$scope.getImageProfile = function(message) {
@@ -34,44 +68,62 @@ function smartphoneFbCtrl($scope) {
 	$scope.getNameProfile = function(message) {
 	//console.log(unescape(message.authorName));	
     return unescape(message.authorName);
-		//return $scope.socialNetwork.getUserProfile(message.socialNetworkId).name;
+	//return $scope.socialNetwork.getUserProfile(message.socialNetworkId).name;
 	}
+	
+	$scope.getDate = function(message) {
+		return message.msgDate;
+	}
+	 
+	$scope.getLastMsg = function(sn){
+      if(sn.alreadyFetched)
+        return sn.lastMessages;
+      sn.alreadyFetched = true;
+      return sn.getLastNMessages(20);
+    }
 }
 
 function smartphoneGpCtrl($scope) {
 	$scope.socialNetwork = $scope.socialNetworks[1];
-	$scope.messages = $scope.socialNetwork.getLastNMessages(20);
+	//$scope.messages = $scope.socialNetwork.getLastNMessages(20);
 	$scope.getIcon = $scope.socialNetwork.icon;
 
 	$scope.getImageProfile = function(message) {
-	var img = message.authorImg;
-  if (img) return img;
-	var img = $scope.socialNetwork.getUserProfile(message.socialNetworkId).imageProfileURL;
-	if (img) return img;
-	return "./img/defaultProfile.png";
+    var img = message.authorImg;
+    if (img) return img;
+		// img = $scope.socialNetwork.getUserProfile(message.socialNetworkId).imageProfileURL;
+		// if (img) return img;
+		return "../common/img/defaultProfile.png";
 	}
-
+	
 	$scope.getNFirstCharacters = function(message, lg) {
 	if (message.msgContent.length > lg)
 		return unescape(message.msgContent.substring(0, lg)) + "....";
 	return unescape(message.msgContent);
 	}
 
-	$scope.getNameProfile = function(message) { 
-    //return unescape(message.authorName);
-		return $scope.socialNetwork.getUserProfile(message.socialNetworkId).name;
+	$scope.getNameProfile = function(message) {
+	//console.log(unescape(message.authorName));	
+    return unescape(message.authorName);
+	//return $scope.socialNetwork.getUserProfile(message.socialNetworkId).name;
 	}
+	
+	    $scope.getLastMsg = function(sn){
+      if(sn.alreadyFetched)
+        return sn.lastMessages;
+      sn.alreadyFetched = true;
+      return sn.getLastNMessages(20);
+    }
 }
 
 function smartphoneTwCtrl($scope) {
 	$scope.socialNetwork = $scope.socialNetworks[2];
-	$scope.messages = $scope.socialNetwork.getLastNMessages(20);
+	//$scope.messages = $scope.socialNetwork.getLastNMessages(20);
 	$scope.getIcon = $scope.socialNetwork.icon;
 
 	$scope.getImageProfile = function(message) {
-	var img = $scope.socialNetwork.getUserProfile(message.socialNetworkId).imageProfileURL;
-	if (img) return img;
-	return "../common/img/defaultProfile.png";
+    var img = message.authorImg;
+    if (img) return img;
 	}
 
 	$scope.getNFirstCharacters = function(message, lg) {
@@ -84,6 +136,17 @@ function smartphoneTwCtrl($scope) {
     return unescape(message.authorName);
 		//return $scope.socialNetwork.getUserProfile(message.socialNetworkId).name;
 	}
+	
+	$scope.getDate = function(message) {
+	return message.msgDate;
+	}
+	
+	$scope.getLastMsg = function(sn){
+      if(sn.alreadyFetched)
+        return sn.lastMessages;
+      sn.alreadyFetched = true;
+      return sn.getLastNMessages(20);
+    }
 }
 
 /** Controleur pour la navigation : page precedente et page suivante **/
@@ -112,9 +175,44 @@ $(document).bind('pageinit', function(event) {
 	}
 }
 
+/** Tri fusion **/
+/* Functions mergeSort & merge : http://www.stoimen.com/blog/2010/07/02/friday-algorithms-javascript-merge-sort/ */
+function mergeSort(arr)
+{
+    if (arr.length < 2)
+        return arr;
+ 
+    var middle = parseInt(arr.length / 2);
+    var left   = arr.slice(0, middle);
+    var right  = arr.slice(middle, arr.length);
+ 
+    return merge(mergeSort(left), mergeSort(right));
+}
+ 
+function merge(left, right)
+{
+    var result = [];
+ 
+    while (left.length && right.length) {
+        if (left[0].msgDate <= right[0].msgDate) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+ 
+    while (left.length)
+        result.push(left.shift());
+ 
+    while (right.length)
+        result.push(right.shift());
+ 
+    return result;
+}
+
 function storeMessage($msg, $authorImg) {
-	/*console.log("Message original : " + $msg);
-	console.log("Auteur original : " + $author);
+	/*console.log("Message original : " + $msg.msgContent);
+	console.log("Auteur original : " + $msg.authorName);
 	*/
 	$('#msgDetails').data('message', escape($msg.msgContent));
 	$('#msgDetails').data('author', escape($msg.authorName));
