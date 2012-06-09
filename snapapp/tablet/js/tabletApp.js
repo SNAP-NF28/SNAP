d3c68e33d20c916926740a89f50de3b2eef7f9ba
Romain Yon <romain.yon@gmail.com>
2012-06-09 15:02:40 +0200

    add pane selection logic

--- snapapp/tablet/js/tabletApp.js
+++ snapapp/tablet/js/tabletApp.js
@@ -3,36 +3,9 @@ angular.module('tabletApp', ['socialNetworks']);
 function tabletAppCtrl($scope, SocialNetworks) {
 
     $scope.socialNetworks = new SocialNetworks();
-
-    $scope.loadProfile = function(socialNetwork) {
-
-        if (socialNetwork.name == 'Facebook') {
-
-            if ($scope.socialNetworks[0].getUserProfile(0).name == 'Charles') {
-                alert('Bonjour ! Bienvenue sur SNAP ');
-            }
-            return $scope.socialNetworks[0].getUserProfile(0);
-
-        } else if (socialNetwork.name == 'Twitter') {
-            return $scope.socialNetworks[2].getUserProfile(2);
-        } else {
-            return $scope.socialNetworks[1].getUserProfile(1);
-        }
-
-    }
-
-    $scope.profileFb = $scope.loadProfile($scope.socialNetworks[0]);
-    $scope.profileG = $scope.loadProfile($scope.socialNetworks[1]);
-    $scope.profileTw = $scope.loadProfile($scope.socialNetworks[2]);
-
-    $scope.getProfile = function (socialNetwork) {
-        if (socialNetwork.name == 'Facebook') {
-            return $scope.profileFb;
-        } else if (socialNetwork.name == 'Twitter') {
-            return $scope.profileTw;
-        } else {
-            return $scope.profileG;
-        }
+    $scope.selectPane = function(selectedPane){
+        $(".sn-pane").addClass('hide');
+        $('#'+selectedPane).removeClass('hide');
     }
 
 }
