9e4e58bcabb1caa8660790dc6e4c700255f9a410
Deheeger Pierrick <pdeheeger@gmail.com>
2012-06-13 21:38:22 +0200

    add subcription date on profile

--- snapapp/common/js/models/mocks/typeMocks.js
+++ snapapp/common/js/models/mocks/typeMocks.js
@@ -11,7 +11,7 @@ factory('MessageMock', function() {
     factory('ProfileMock', function() {
         var ProfileMock = function(){
 			//this.imageProfileURL = "https://twimg0-a.akamaihd.net/profile_images/1126933087/fef61c23219a521010627333af7bbadd9c398dd770e4978facb12c666b2be326e56f045cfull.jpg";
-        	this.imageProfileURL = "/snapapp/commun/img/defaultProfile.png";
+        	this.imageProfileURL = "/snapapp/common/img/defaultProfile.png";
         	this.name = "Charles";
         };
 
