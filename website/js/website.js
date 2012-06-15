f90911c7383680c45d90f9ebf3b87a9ccbb9d1b1
Kim-Adeline Miguel <kimadeline.miguel@gmail.com>
2012-06-15 03:51:47 +0200

    add toggle en/fr + header

--- website/js/website.js
+++ website/js/website.js
@@ -1,2 +1,20 @@
 /** website.js **/
 
+$(document).ready(function () {
+  toggle(false);
+});
+
+/** Toggle en/fr **/
+function toggle(bool) {
+	if (bool) { /* English */
+		console.log("english spoken here");
+		$(".fra").hide();
+		$(".eng").show();
+	} else { /* Francais */
+		console.log("on parle francais ici");
+		$(".eng").hide();
+		$(".fra").show();
+	}
+
+}
+
