ee04b8f6aee4bb2e140bb3636f9e825fcf4632de
Romain Yon <romain.yon@gmail.com>
2012-06-17 00:19:17 +0200

    clean some crap + detect ipad

--- snapapp/common/js/load.js
+++ snapapp/common/js/load.js
@@ -6,7 +6,6 @@ commonScripts = [
     "order!http://platform.twitter.com/anywhere.js?id=HDz4lPcTE9tIMD3SV3Nkg&v=1",
     "order!http://connect.facebook.net/en_US/all.js",
     "order!https://apis.google.com/js/client.js",
-    "order!/snapapp/common/js/models/facebook/facebookConnect.js",
     "order!/snapapp/common/js/models/google+/google+.js",
     "order!/snapapp/common/js/models/mocks/SNMocks.js",
     "order!/snapapp/common/js/models/mocks/typeMocks.js",
